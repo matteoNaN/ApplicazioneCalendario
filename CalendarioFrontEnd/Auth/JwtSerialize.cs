8e2689edc7378e2875b6e59f9d8790f2182ec3fb
matteoNaN <matteonanni9897@gmail.com>
2025-02-08 19:19:43 +0100

    finito parte di login e registrazione
    finita la parte di login e registrazione, messo a posto il routing per le pagine che richiedo authentificazione

--- CalendarioFrontEnd/Auth/JwtSerialize.cs
+++ CalendarioFrontEnd/Auth/JwtSerialize.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Text.Json.Nodes;
 using System.Text.Json;
 using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace CalendarioFrontEnd.Auth
 {
@@ -10,63 +11,53 @@ namespace CalendarioFrontEnd.Auth
     {
         public static ClaimsPrincipal Deserialize(string jwtToken)
         {
-            var segments = jwtToken.Split('.');
+            var handler = new JwtSecurityTokenHandler();
 
-            if (segments.Length != 3)
-            {
-                throw new Exception("Invalid JWT");
-            }
+            
+            var jsonToken = handler.ReadJwtToken(jwtToken);
 
-            Console.WriteLine(segments[1]);
-            var dataSegment = Encoding.UTF8.GetString(FromUrlBase64(segments[1]));
-            var data = JsonSerializer.Deserialize<JsonObject>(dataSegment);
+            
+            var claims = jsonToken.Claims.ToArray();
 
-            var claims = new Claim[data.Count];
-            int index = 0;
-            foreach (var entry in data)
-            {
-                claims[index] = JwtNodeToClaim(entry.Key, entry.Value);
-                index++;
-            }
-
+            
             var claimIdentity = new ClaimsIdentity(claims, "jwt");
             var principal = new ClaimsPrincipal(new[] { claimIdentity });
 
             return principal;
         }
 
-        private static Claim JwtNodeToClaim(string key, JsonNode node)
-        {
-            var jsonValue = node.AsValue();
+        //private static Claim JwtNodeToClaim(string key, JsonNode node)
+        //{
+        //    var jsonValue = node.AsValue();
 
-            if (jsonValue.TryGetValue<string>(out var str))
-            {
-                return new Claim(key, str, ClaimValueTypes.String);
-            }
-            else if (jsonValue.TryGetValue<double>(out var num))
-            {
-                return new Claim(key, num.ToString(CultureInfo.InvariantCulture), ClaimValueTypes.Double);
-            }
-            else
-            {
-                throw new Exception("Unsupported JWT claim type");
-            }
-        }
+        //    if (jsonValue.TryGetValue<string>(out var str))
+        //    {
+        //        return new Claim(key, str, ClaimValueTypes.String);
+        //    }
+        //    else if (jsonValue.TryGetValue<double>(out var num))
+        //    {
+        //        return new Claim(key, num.ToString(CultureInfo.InvariantCulture), ClaimValueTypes.Double);
+        //    }
+        //    else
+        //    {
+        //        throw new Exception("Unsupported JWT claim type");
+        //    }
+        //}
 
-        private static byte[] FromUrlBase64(string jwtSegment)
-        {
-            string fixedBase64 = jwtSegment
-                .Replace('-', '+')
-                .Replace('_', '/');
+        //private static byte[] FromUrlBase64(string jwtSegment)
+        //{
+        //    string fixedBase64 = jwtSegment
+        //        .Replace('-', '+')
+        //        .Replace('_', '/');
 
-            switch (jwtSegment.Length % 4)
-            {
-                case 2: fixedBase64 += "=="; break;
-                case 3: fixedBase64 += "="; break;
-                default: throw new Exception("Illegal base64url string!");
-            }
+        //    switch (jwtSegment.Length % 4)
+        //    {
+        //        case 2: fixedBase64 += "=="; break;
+        //        case 3: fixedBase64 += "="; break;
+        //        default: throw new Exception("Illegal base64url string!");
+        //    }
 
-            return Convert.FromBase64String(fixedBase64);
-        }
+        //    return Convert.FromBase64String(fixedBase64);
+        //}
     }
 }
