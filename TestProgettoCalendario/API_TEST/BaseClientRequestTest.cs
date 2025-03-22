feebb040a4acb1e3cef3b04252e757b50e2ddde3
matteoNaN <matteonanni9897@gmail.com>
2025-03-22 01:30:43 +0100

    Aggiungi progetto di test e migliora la gestione degli impegni
    - Aggiunto il progetto "TestProgettoCalendario" nella soluzione.
    - Implementata una nuova modale in `CalendarioGruppo.razor` per visualizzare gli impegni.
    - Creato il componente `ImpegniComponent.razor` per mostrare i dettagli degli impegni.
    - Aggiornata la configurazione del progetto di test per .NET 9.0 e aggiunte dipendenze necessarie.
    - Aggiunto `BaseClientRequestTest.cs` per facilitare i test delle richieste client.

---
+++ TestProgettoCalendario/API_TEST/BaseClientRequestTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgettoCalendario.API_TEST
+{
+    public class BaseClientRequestTest : WebApplicationFactory<Program>
+    {
+        
+    }
+}
